9cf93bf641ee09c8cbc36f73ac3fe67830bccf5a
Ben Peachey <potherca@gmail.com>
2016-10-26 20:33:49 +0000

    CHANGE: Makes HelloTest use the Locator.

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -23,7 +23,33 @@ class HelloTest extends \PHPUnit_Framework_TestCase
 
     final public function setUp()
     {
-        $this->hello = new Hello();
+        $path = dirname(__DIR__).'/subject';
+
+        if (is_dir($path)) {
+            $adapter = new \League\Flysystem\Adapter\Local($path);
+            $fileSystem = new \League\Flysystem\Filesystem($adapter);
+            $locator = new Locator($fileSystem);
+
+            $file = $locator->locate('Hello.php');
+
+            $classesBeforeLoad = get_declared_classes();
+
+            include sprintf('%s/%s', $path, $file['path']);
+
+            $classesAftterLoad = get_declared_classes();
+
+            $loadedClasses = array_values(
+                array_diff_key(
+                    $classesAftterLoad,
+                    $classesBeforeLoad
+                )
+            );
+
+            $this->hello = new $loadedClasses[0];
+
+        } else {
+            $this->hello = new Hello();
+        }
     }
 
     final public function testHelloShouldReturnHelloWorldWhenAskedTo()
