63e55973b94ebc0dadd2df768847488a39c845fb
Ben Peachey <potherca@gmail.com>
2016-10-26 20:53:42 +0000

    CHANGE: Removes comment from test class.

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -2,20 +2,6 @@
 
 namespace Potherca\Examples;
 
-/*
-
-There is a Foo class (as an example) and there is a FooTest class.
-The Foo classs implements a FooInterface. The tests should be written against the interface.
-
-The idea is to give developers the interface and ask them to implement the class, including tests.
-
-When they are content, we receive the class and run *our* tests against their class.
-
-This is done by injecting the class into the test.
-
-The simpelest way to do this is to extend the test, locate the developers class,
-instantiate it, set it on the TestClass as a property and have everything execute as normal.
-*/
 class HelloTest extends \PHPUnit_Framework_TestCase
 {
     /** @var HelloInterface */
