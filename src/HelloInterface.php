1948c1813c2ff6347d9d220b51081a26f6fb6d07
Ben Peachey <potherca@gmail.com>
2016-10-26 20:31:38 +0000

    ADD: Adds HelloInterface.

---
+++ src/HelloInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Potherca\Examples;
+
+interface HelloInterface
+{
+    const WORLD = 'World';
+
+    /**
+     * @return string
+     */
+    public function world();
+}
+
+/*EOF*/
